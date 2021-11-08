c8c1538ab634dafce23581d79822ce3db8bc964e
mhrrosa <71407848+mhrrosa@users.noreply.github.com>
2021-11-07 23:19:47 -0300

    reza que o 100 vem

--- OLART/public_html/JS/Home.js
+++ OLART/public_html/JS/Home.js
@@ -1,6 +1,8 @@
 function atualizar() {
+   
     src = "";
     dados = JSON.parse(window.localStorage.getItem("dados_produtos"));
+
     for (var i = 0; i < dados.length; i++) {
         src = dados[i][3];
         document.getElementById("gerar").innerHTML += "<div class=card-externo id=" + i + ">\n\
@@ -30,7 +32,7 @@ function atualizar() {
             </div>\n\
         </div>\n\
                         ";
-        console.log(i);
+        console.log('aaaaaaaaaa');
     }
     VerificaId();
 }
@@ -42,7 +44,7 @@ function VerificaId() {
 };
 
 window.onload = function() {
-    // atualizar();
+    atualizar();
     gerarCardDinamico();
 };
 
@@ -57,35 +59,36 @@ var artes = [
     [3, "O grito", "O Grito é uma série de quatro pinturas do norueguês Edvard Munch, 1893. A obra representa uma figura andrógina num momento de profunda angústia e desespero. O plano de fundo é a doca do fiorde de Oslo ao pôr do sol", "120Mi"],
 
     [4, "Abaporu", "Abaporu é uma pintura a óleo da artista brasileira Tarsila do Amaral. É uma das principais obras do período antropofágico do movimento modernista no Brasil.", "40Mi"]
-]
+    ];
 
 function gerarCardDinamico() {
 
-    console.log("teste")
+
     for (let i = 0; i < artes.length; i++) {
 
-        count += '<div class="card-alinhar">';
-        count += '<div class="card-externo">';
-        count += '<div class="card-interno">';
-        count += '<div class="carousel slide>';
-        count += '<img class="img-cadastro" src="../imagens/artes/' + '2.png' + ' > ';
+        
+        count += '<div class=card-alinhar>';
+        count += '<div class=card-externo>';
+        count += '<div class=card-interno>';
+        count += '<div class=carousel slide>';
+        count += '<img class=img-cadastro src=../imagens/artes/' + i+'.png' + ' > ';
         count += '</div>';
-        count += '<div class="infos" id="id_infos">';
-        count += '<h2 class="titulo-arte">' + 'artes' + '</h2>';
-        count += '<hr class="separador-card">';
-        count += '<h2 class="descricao-titulo">Sobre</h2>';
-        count += '<div class="box-descricao">';
-        count += '<p class="descricao-card">A arte é uma representação</p>';
+        count += '<div class=infos id="id_infos">';
+        count += '<h2 class=titulo-arte">' + artes[i][1] + '</h2>';
+        count += '<hr class=separador-card>';
+        count += '<h2 class=descricao-titulo> descricao </h2>';
+        count += '<div class=box-descricao>';
+        count += '<p class=descricao-card>'+artes[i][2]+'</p>';
         count += '</div>';
-        count += '<h2 class="preco">R$ 1.000</h2>';
+        count += '<h2 class=preco>'+artes[i][3]+'</h2>';
         count += '</div>';
         count += '</div>';
-        count += '<div class="box-adicionar-carrinho">';
-        count += '<button class="adicionar-carrinho"><img class="carrinho" src="../imagens/carrinho.png" alt="">Adicionar ao carrinho</button>';
+        count += '<div class=box-adicionar-carrinho>';
+        count += '<button class=adicionar-carrinho><img class=carrinho src=../imagens/carrinho.png alt="">Adicionar ao carrinho</button>';
         count += '</div>';
         count += '</div>';
         count += '</div>';
     }
 
-    document.getElementById("gerar").innerHTML = count;
+    document.getElementById("gerarcards").innerHTML = count;
 }
